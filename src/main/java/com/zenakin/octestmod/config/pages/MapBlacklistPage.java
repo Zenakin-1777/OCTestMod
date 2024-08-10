a5642637b0858e0a3cd77c27f5a1e8a9ebd7f1d2
Zenakin-1777 <154721632+Zenakin-1777@users.noreply.github.com>
2024-08-10 16:56:05 -0400

    another commit

--- src/main/java/com/zenakin/octestmod/config/pages/MapBlacklistPage.java
+++ src/main/java/com/zenakin/octestmod/config/pages/MapBlacklistPage.java
@@ -10,7 +10,7 @@ import cc.polyfrost.oneconfig.config.annotations.Switch;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapBlacklistPage extends Page{
+public class MapBlacklistPage {
     public List<String> blacklistedMaps = new ArrayList<>();
 
     @Switch(
@@ -39,10 +39,6 @@ public class MapBlacklistPage extends Page{
     )
     Runnable runnable = this::saveSwitchStates;
 
-    public MapBlacklistPage() {
-        super("Blacklisted Maps");
-    }
-
     public void onToggle(String mapName, boolean isToggled) {
         if (isToggled) {
             blacklistedMaps.add(mapName);
@@ -57,21 +53,5 @@ public class MapBlacklistPage extends Page{
         onToggle("Map 2", map2);
         onToggle("Map 3", map3);
         onToggle("Map 4", map4);
-    }
-
-    @Override
-    public void draw(long vg, int x, int y, InputHandler inputHandler) {
-        int startX = x + 16;
-        int startY = y + 16;
-        int gridX = x + 16;
-        int gridY = y + 16 + 48 + 16;
-    }
-
-    public int drawStatic(long vg, int x, int y) {
-        return 12;
-    }
-
-    public int getMaxScrollHeight() {
-        return 1240;
     }
 }
